70daae769c1e175f600d5886bdfcd37c52600d2e
mperelli <101881175+matiasperelli@users.noreply.github.com>
2024-05-03 18:43:02 -0400

    Create show your number.c

---
+++ show your number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void number(){
+    int n;
+    printf("Put an integer number: \n");
+    scanf("%d", &n);
+    printf("%d", n);
+
+}
+
+int main() {
+
+    number();
+
+    return 0;
+}
